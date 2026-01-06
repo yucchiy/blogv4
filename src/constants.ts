05687125e4d4e188d567c914a2c8052737945fc7
Yuichiro MUKAI <y.iky917@gmail.com>
2026-01-06 09:52:05 +0900

    ソーシャルアイコンをSimple Iconsに移行し、はてなブックマークを追加
    - Simple Iconsパッケージを導入
    - X、Facebook、GitHubアイコンをSimple Iconsに移行
    - はてなブックマークのシェアボタンを追加（Xの次に配置）
    - アイコンスタイル（outline/filled）に応じたCSSを適用
    - GLightboxのピンチズーム機能を有効化

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,10 @@
 import type { Props } from "astro";
 import IconMail from "@/assets/icons/IconMail.svg";
-import IconGitHub from "@/assets/icons/IconGitHub.svg";
-import IconBrandX from "@/assets/icons/IconBrandX.svg";
 import IconLinkedin from "@/assets/icons/IconLinkedin.svg";
-import IconFacebook from "@/assets/icons/IconFacebook.svg";
+import IconGitHub from "simple-icons/icons/github.svg";
+import IconBrandX from "simple-icons/icons/x.svg";
+import IconFacebook from "simple-icons/icons/facebook.svg";
+import IconHatenabookmark from "simple-icons/icons/hatenabookmark.svg";
 import { SITE } from "@/config";
 
 interface Social {
@@ -11,6 +12,7 @@ interface Social {
   href: string;
   linkTitle: string;
   icon: (_props: Props) => Element;
+  iconStyle: "outline" | "filled";
 }
 
 export const SOCIALS: Social[] = [
@@ -19,18 +21,21 @@ export const SOCIALS: Social[] = [
     href: "https://github.com/yucchiy",
     linkTitle: `${SITE.title} on GitHub`,
     icon: IconGitHub,
+    iconStyle: "filled",
   },
   {
     name: "X",
     href: "https://x.com/yucchiy_",
     linkTitle: `${SITE.title} on X`,
     icon: IconBrandX,
+    iconStyle: "filled",
   },
   {
     name: "LinkedIn",
     href: "https://www.linkedin.com/in/yucchiy/",
     linkTitle: `${SITE.title} on LinkedIn`,
     icon: IconLinkedin,
+    iconStyle: "outline",
   },
 ] as const;
 
@@ -40,23 +45,34 @@ export const SHARE_LINKS: Social[] = [
     href: "https://x.com/intent/post?url=",
     linkTitle: `Share this post on X`,
     icon: IconBrandX,
+    iconStyle: "filled",
+  },
+  {
+    name: "Hatena Bookmark",
+    href: "https://b.hatena.ne.jp/entry/",
+    linkTitle: `Share this post on Hatena Bookmark`,
+    icon: IconHatenabookmark,
+    iconStyle: "filled",
   },
   {
     name: "Facebook",
     href: "https://www.facebook.com/sharer.php?u=",
     linkTitle: `Share this post on Facebook`,
     icon: IconFacebook,
+    iconStyle: "filled",
   },
   {
     name: "LinkedIn",
     href: "https://www.linkedin.com/sharing/share-offsite/?url=",
     linkTitle: `Share this post on LinkedIn`,
     icon: IconLinkedin,
+    iconStyle: "outline",
   },
   {
     name: "Mail",
     href: "mailto:?subject=See%20this%20post&body=",
     linkTitle: `Share this post via email`,
     icon: IconMail,
+    iconStyle: "outline",
   },
 ] as const;
